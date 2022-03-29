6454044f37f2b0aea18ccf46b065071c3073aa43
Naatchi <joshpahwa2@gmail.com>
2022-03-29 12:19:03 -0400

    cleaned components and removed old files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,19 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import { useAuth } from '../hooks/auth'
-import style from '../styles/styles.module.css'
-import waves from "../components/wave/waves";
-import Waves from "../components/wave/waves";
+import ApplicationLogo from "../components/ApplicationLogo";
 
 export default function Home() {
-    const { user } = useAuth({ middleware: 'guest', redirectIfAuthenticated: false })
-
-    // @ts-ignore
+    // const { user } = useAuth({ middleware: 'guest', redirectIfAuthenticated: false })
     return (
         <>
             <Head>
                 <title>Laravel</title>
             </Head>
-            <section id="start" className="bg-gray-900">
-                <div id="spacing" className="h-36"/>
-                <div className="grid place-items-center h-screen text-white">
-                    <div>
-                        <a className="align-middle">
-                            i love fornite
-                        </a>
-                        <img src='https://media.giphy.com/media/SG5paY6WxH6Ki2lWys/giphy.gif' />
-                    </div>
-                    <div>
-                        <img src='/images/icons/peaks-newres.svg' className=" w-screen overflow-hidden"/>
-                        <section style={{backgroundColor: '#6F1303'}}>
-                            this is a section
-                        </section>
+            <section id="wrapper" className="bg-gray-900">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="flex justify-center items-center h-screen">
+                        <a className="text-6xl text-white font-semibold"> Text :) </a>
                     </div>
                 </div>
             </section>
