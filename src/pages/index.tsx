2210abc49c1126679388512fe07582414678910b
Naatchi <joshpahwa2@gmail.com>
2022-03-31 10:37:14 -0400

    updated dependencies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,4 @@
 import Head from 'next/head'
-import Link from 'next/link'
-import { useAuth } from '../hooks/auth'
-import ApplicationLogo from "../components/ApplicationLogo";
 
 export default function Home() {
     // const { user } = useAuth({ middleware: 'guest', redirectIfAuthenticated: false })
@@ -10,11 +7,9 @@ export default function Home() {
             <Head>
                 <title>Laravel</title>
             </Head>
-            <section id="wrapper" className="bg-gray-900">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="flex justify-center items-center h-screen">
-                        <a className="text-6xl text-white font-semibold"> Text :) </a>
-                    </div>
+            <section>
+                <div>
+                    text :)
                 </div>
             </section>
         </>
