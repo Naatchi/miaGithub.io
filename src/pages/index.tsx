58576008dfb5b42460bce303e023bf118bdf89d2
Naatchi <joshpahwa2@gmail.com>
2022-03-31 12:54:27 -0400

    ok deps ACTUALLY updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,15 @@
 import Head from 'next/head'
+import { Parallax, ParallaxLayer} from "@react-spring/parallax";
 
 export default function Home() {
     // const { user } = useAuth({ middleware: 'guest', redirectIfAuthenticated: false })
+
+    const urlBg:string = "https://mineinabyss.com/assets/images/render/mia_render.jpg"
     return (
         <>
             <Head>
                 <title>Laravel</title>
             </Head>
-            <section>
-                <div>
-                    text :)
-                </div>
-            </section>
         </>
     )
 }
