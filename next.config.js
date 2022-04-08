b249e267a9bd6565699e758a5d05866a69e12a23
Naatchi <joshpahwa2@gmail.com>
2022-04-07 22:06:28 -0400

    fixed tailwind :)

--- next.config.js
+++ next.config.js
@@ -3,5 +3,8 @@ const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
     assetPrefix: isProd ? '/mia-github.io' : '',
     basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
+    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+    plugins: [
+        require('tailwindcss')
+    ]
 }
