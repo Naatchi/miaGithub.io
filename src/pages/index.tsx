5e8c82ea2f9f672e2238eb6360e9033dcb609272
Naatchi <joshpahwa2@gmail.com>
2022-04-09 14:49:35 -0400

    cleaned up some code and added a REALLY BAD footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,59 +18,55 @@ export default function Home() {
         }
     })
 
-    const array = [
-        {url: "https://www.firewatchgame.com/images/parallax/parallax0.png",pr: 0.95},
-        {url: "https://www.firewatchgame.com/images/parallax/parallax1.png", pr: 0.85},
-        {url: "https://www.firewatchgame.com/images/parallax/parallax2.png",pr: 0.81},
-        {url: "https://www.firewatchgame.com/images/parallax/parallax3.png",pr: 0.75},
-        {url: "https://www.firewatchgame.com/images/parallax/parallax4.png",pr: 0.65},
-        {url: "https://www.firewatchgame.com/images/parallax/parallax5.png",pr: 0.5},
-        {url: "https://www.firewatchgame.com/images/parallax/parallax6.png",pr: 0.3},
-        {url: "https://www.firewatchgame.com/images/parallax/parallax7.png",pr: 0.3},
+    const array = [ //FFAF1B 210002
+        {url: "https://www.firewatchgame.com/images/parallax/parallax0.png",pr: 1.00},
+        {url: "https://www.firewatchgame.com/images/parallax/parallax1.png", pr: 0.875},
+        {url: "https://www.firewatchgame.com/images/parallax/parallax2.png",pr: 0.75},
+        {url: "https://www.firewatchgame.com/images/parallax/parallax3.png",pr: 0.625},
+        {url: "https://www.firewatchgame.com/images/parallax/parallax4.png",pr: 0.5},
+        {url: "https://www.firewatchgame.com/images/parallax/parallax5.png",pr: 0.375},
+        {url: "https://www.firewatchgame.com/images/parallax/parallax6.png",pr: 0.25},
+        {url: "https://www.firewatchgame.com/images/parallax/parallax7.png",pr: 0.125},
         {url: "https://www.firewatchgame.com/images/parallax/parallax8.png", pr: 0}
     ]
 
     return (
         <>
-            <section id="wrapper" style={{backgroundColor: '#FFAF1B'}}>
-                <section id="parallax">
-                    {
-                        array.map(a => {
-                            return (
-                                <div
-                                    style={{
-                                        backgroundImage: `url(${a.url})`,
-                                        transform: `translateY(${scrollY*a.pr}px)`,
-                                        backgroundSize: 'auto 1000px',
-                                        backgroundRepeat:'repeat-x',
-                                        width: '100%',
-                                        position: 'absolute',
-                                        backgroundPosition: 'bottom center',
-                                        height: '1000px',
-                                        transition: "all 0.5s ease-in-out"
-                                    }}
-                                />
-                            )
-                        })
-                    }
-                </section>
-                <section id="site">
-                    <section id="abyss-1" style={{height: 2000, paddingTop: 1000}}>
-                        <div id="bg-container" className="relative h-screen" style={{backgroundColor: '#280003', paddingBottom: 2546}}>
-                            <section className="p-2">
-                                <a className="text-white">
-                                    hello! this is a test site that i am using to test <br/>
-                                    parallax utilizing nextjs!<br/>
-                                    if you would like to see the repository <br/>
-                                    for this website
-                                    <a href="https://github.com/Naatchi/miaGithub.io" className="text-blue-600 hover:underline" title="source-code"> click here! </a> <br/>
-                                    all art belongs to <a href="https://www.camposanto.com/" className="text-blue-600 hover:underline" title="Campo-santo's website!"> campo santo </a> and <br/>
-                                    comes directly from <a href="https://www.firewatchgame.com/" className="text-blue-600 hover:underline" title="firewatch game website!"> the firewatchgame website </a>
-                                </a>
-                            </section>
-                        </div>
-                    </section>
-                </section>
+            <section style={{backgroundColor: '#FFAF1B', height: 1030}}>
+                {
+                    array.map(a => {
+                        return (
+                            <div
+                                key={a.url}
+                                className="bg-center bg-cover absolute"
+                                style={{
+                                    //transform: `translateY(${scrollY* a.pr}px)`,
+                                    backgroundImage: `url(${a.url})`,
+                                    backgroundPositionY: scrollY * a.pr,
+                                    backgroundRepeat:'repeat-x',
+                                    position: 'absolute',
+                                    width: '100%',
+                                    height: 1030,
+                                    transition: '0.8s ease-in-out'
+                                }}
+                            />
+                        )
+                    })
+                }
+            </section>
+            <section style={{backgroundColor: '#210002', position:"relative"}}>
+                <div className="h-screen lg:px-96 sm:px-6 text-white text-md">
+                    Hello! this is a site for testing parallax <br/>
+                    utilizing nextjs and react <br/>
+                    all art belongs to <a className="text-blue-500 hover:underline" href="https://www.camposanto.com/" title="https://www.camposanto.com/"> campo santo</a> <br/>
+                    and the <a className="text-blue-500 hover:underline" href="https://www.firewatchgame.com/" title="https://www.firewatchgame.com/"> firewatch team </a> <br/>
+                    <br/>
+                    if you would like to check out <br/>
+                    the source code you <br/>
+                    can find it on this <a className="text-blue-500 hover:underline" href="https://github.com/Naatchi/miaGithub.io" title="https://github.com/Naatchi/miaGithub.io"> repository </a> <br/>
+                    <br/>
+                    reverse engineered with love 💕
+                </div>
             </section>
         </>
     )
