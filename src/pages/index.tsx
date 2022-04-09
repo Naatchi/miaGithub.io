9f2c4b9b50d5d24c6e355b2bdeaede32df897027
Naatchi <joshpahwa2@gmail.com>
2022-04-09 12:26:13 -0400

    added some css transitions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,24 +25,14 @@ export default function Home() {
         {url: "https://www.firewatchgame.com/images/parallax/parallax3.png",pr: 0.75},
         {url: "https://www.firewatchgame.com/images/parallax/parallax4.png",pr: 0.65},
         {url: "https://www.firewatchgame.com/images/parallax/parallax5.png",pr: 0.5},
-        {url: "https://www.firewatchgame.com/images/parallax/parallax6.png",pr: 0.0},
-        {url: "https://www.firewatchgame.com/images/parallax/parallax7.png",pr: 0}
+        {url: "https://www.firewatchgame.com/images/parallax/parallax6.png",pr: 0.3},
+        {url: "https://www.firewatchgame.com/images/parallax/parallax7.png",pr: 0.3},
+        {url: "https://www.firewatchgame.com/images/parallax/parallax8.png", pr: 0}
     ]
 
     return (
         <>
             <section id="wrapper" style={{backgroundColor: '#FFAF1B'}}>
-                <section className="p-2">
-                    <a className="text-black">
-                        hello! this is a test site that i am using to test <br/>
-                        parallax utilizing nextjs!<br/>
-                        if you would like to see the repository <br/>
-                        for this website
-                        <a href="https://github.com/Naatchi/miaGithub.io" className="text-blue-600 hover:underline" title="source-code"> click here! </a> <br/>
-                        all art belongs to <a href="https://www.camposanto.com/" className="text-blue-600 hover:underline" title="Campo-santo's website!"> campo santo </a> and <br/>
-                        comes directly from <a href="https://www.firewatchgame.com/" className="text-blue-600 hover:underline" title="firewatch game website!"> the firewatchgame website </a>
-                    </a>
-                </section>
                 <section id="parallax">
                     {
                         array.map(a => {
@@ -57,6 +47,7 @@ export default function Home() {
                                         position: 'absolute',
                                         backgroundPosition: 'bottom center',
                                         height: '1000px',
+                                        transition: "all 0.5s ease-in-out"
                                     }}
                                 />
                             )
@@ -66,6 +57,17 @@ export default function Home() {
                 <section id="site">
                     <section id="abyss-1" style={{height: 2000, paddingTop: 1000}}>
                         <div id="bg-container" className="relative h-screen" style={{backgroundColor: '#280003', paddingBottom: 2546}}>
+                            <section className="p-2">
+                                <a className="text-white">
+                                    hello! this is a test site that i am using to test <br/>
+                                    parallax utilizing nextjs!<br/>
+                                    if you would like to see the repository <br/>
+                                    for this website
+                                    <a href="https://github.com/Naatchi/miaGithub.io" className="text-blue-600 hover:underline" title="source-code"> click here! </a> <br/>
+                                    all art belongs to <a href="https://www.camposanto.com/" className="text-blue-600 hover:underline" title="Campo-santo's website!"> campo santo </a> and <br/>
+                                    comes directly from <a href="https://www.firewatchgame.com/" className="text-blue-600 hover:underline" title="firewatch game website!"> the firewatchgame website </a>
+                                </a>
+                            </section>
                         </div>
                     </section>
                 </section>
